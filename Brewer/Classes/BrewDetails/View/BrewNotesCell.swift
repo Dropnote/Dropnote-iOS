be49e0184e4764fb65c3e63f4a42af40415151b0
Maciej Oczko <maciejoczko@gmail.com>
2017-04-08 12:19:46 +0200

    Fix layout to notes cell

--- Brewer/Classes/BrewDetails/View/BrewNotesCell.swift
+++ Brewer/Classes/BrewDetails/View/BrewNotesCell.swift
@@ -8,10 +8,19 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 final class BrewNotesCell: UITableViewCell, Highlightable {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var valueLabel: UILabel!
+    lazy var titleLabel: UILabel = UILabel()
+    lazy var valueLabel: UILabel = UILabel()
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.titleLabel, self.valueLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 10.0
+        return stackView
+    }()
     
     var normalColor: UIColor?
     var highlightColor: UIColor?
@@ -19,6 +28,27 @@ final class BrewNotesCell: UITableViewCell, Highlightable {
     override var isHighlighted: Bool {
         didSet {
             highlightViews([self, titleLabel, valueLabel], highlighted: isHighlighted)
+        }
+    }
+
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(stackView)
+        valueLabel.numberOfLines = 0
+        configureConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureConstraints() {
+        stackView.snp.makeConstraints {
+            make in
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
+            make.top.equalToSuperview().offset(10)
+            make.bottom.equalToSuperview().offset(-10)
         }
     }
 }
@@ -37,7 +67,7 @@ extension BrewNotesCell {
     func configureWithTheme(_ theme: ThemeConfiguration?) {
         super.configureWithTheme(theme)
         [titleLabel, valueLabel].forEach {
-            $0!.configureWithTheme(theme)
+            $0.configureWithTheme(theme)
         }
         normalColor = theme?.lightColor
         highlightColor = highlightColorForTheme(theme)
